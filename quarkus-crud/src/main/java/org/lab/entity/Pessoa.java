eaecbfff1543a867ff7f20fbfb8c5e01d1dbc43a
Herberson Miranda <herberson.miranda@outlook.com>
2023-06-22 12:09:07 -0300

    update

--- quarkus-crud/src/main/java/org/lab/entity/Pessoa.java
+++ quarkus-crud/src/main/java/org/lab/entity/Pessoa.java
@@ -24,7 +24,7 @@ public class Pessoa extends PanacheEntity {
 
     public String getNome() {
         return this.nome;
-    }
+    } 
 
     public void setNome(String nome) {
         this.nome = nome;
